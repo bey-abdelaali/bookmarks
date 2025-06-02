b01ceff5edf5ed85b0bfc3e4d7e16cb2606dda4d
bey abdelaali <bey.abdelaali@gmail.com>
2025-06-02 10:46:05 +0100

    User Registration

--- account/urls.py
+++ account/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),    
     path('', include('django.contrib.auth.urls')), 
     path('dashboard/', views.dashboard, name='dashboard'), 
+    path('register/', views.register, name='register'), 
 ]
