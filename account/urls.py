77517ceefffd0869c1c05bccee769864f26795f3
bey abdelaali <bey.abdelaali@gmail.com>
2025-06-05 23:03:42 +0100

    User Profile Edit

--- account/urls.py
+++ account/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')), 
     path('dashboard/', views.dashboard, name='dashboard'), 
     path('register/', views.register, name='register'), 
+    path('edit/', views.edit, name='edit'), 
 ]
